3f72c8fb821db76c88840d0a31690797c9ceee84
SushilSingh3 <sushilkrsingh777@gmail.com>
2025-01-27 17:35:04 +0530

    Your commit message

--- Backend/attendance-management-backend/config/db.js
+++ Backend/attendance-management-backend/config/db.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     process.exit(1); // Exit the app if the database connection fails
-//   }
-// };
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit the app if the database connection fails
+  }
+};
 
-const connectDB = mongoose.connect(process.env.MONGO_URI).then(()=>{
-  console.log("connected")
-})
+// const connectDB = mongoose.connect(process.env.MONGO_URI).then(()=>{
+//   console.log("connected")
+// })
 
 module.exports = connectDB;
