6f16a53c63f66af16c3ca3d05eab03683ce06e79
Sujal Raj <sujalraj2064@gmail.com>
2025-01-28 17:43:02 +0530

    AdminDashboard Intialized

--- Frontend/Attendance management system/Pages/AdminDashboard.jsx
+++ Frontend/Attendance management system/Pages/AdminDashboard.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function AdminDashboard() {
   return (
     <>
-      
+      <nav className='h-[10vh] text-4xl font-bold flex items-center justify-center'>
+        <p>Admin Dashboard</p>
+      </nav>
     </>
   )
 }
